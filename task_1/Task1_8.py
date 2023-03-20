9fa8eada01a147bfabaf0f633a24e6f730c68f9c
oxanaalkh <ksenikrsk@mail.ru>
2023-03-20 10:00:13 +0300

    HW8 add

---
+++ task_1/Task1_8.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+path = Path("files", "info", "info_1.txt")
+
+# выведем значение переменной path:
+print(str(path))
---
+++ task_2/main.py
@@ -0,0 +1,48 @@
+"""
+2. Задание на закрепление знаний по модулю json. Есть файл orders
+в формате JSON с информацией о заказах. Написать скрипт, автоматизирующий
+его заполнение данными.
+
+Для этого:
+Создать функцию write_order_to_json(), в которую передается
+5 параметров — товар (item), количество (quantity), цена (price),
+покупатель (buyer), дата (date). Функция должна предусматривать запись
+данных в виде словаря в файл orders.json. При записи данных указать
+величину отступа в 4 пробельных символа;
+Проверить работу программы через вызов функции write_order_to_json()
+с передачей в нее значений каждого параметра.
+
+ПРОШУ ВАС НЕ УДАЛЯТЬ ИСХОДНЫЙ JSON-ФАЙЛ
+ПРИМЕР ТОГО, ЧТО ДОЛЖНО ПОЛУЧИТЬСЯ
+
+{
+    "orders": []
+}
+
+вам нужно подгрузить JSON-объект
+и достучаться до списка, который и нужно пополнять
+а потом сохранять все в файл
+"""
+
+import json
+
+filename = 'orders.json'
+
+
+def write_order_to_json(item, quantity, price, buyer, date):
+    with open(filename, encoding="utf-8") as fl:
+        dict_j = json.loads(fl.read())
+
+    dict_j['orders'].append(
+        {'item': item, 'quantity': quantity, 'price': price,
+         'buyer': buyer, 'date': date})
+
+    with open(filename, "w", encoding="utf-8") as fl:
+        json.dump(dict_j, fl, indent=4, separators=(',', ': '),
+                  ensure_ascii=False)
+
+    print(f'Данные добавлены в {filename}')
+
+
+write_order_to_json('Компьютер', '1', '567', 'Жерновой',
+                    '30.04.2023')
